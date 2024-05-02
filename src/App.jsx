0cbc6f4f2b5b6995e503e6c650aeb4ed167af7e7
irinbenny <irinbenny30@gmail.com>
2024-05-02 14:30:02 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
-import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
+import CategoryManagement from './components/CategoryManagement';
+import ProductManagement from './components/ProductManagement';
+import DashBoardManagment from './components/DashBoardManagment';
+import AdminAccount from './components/AdminAccount';
+import Admin from './pages/Admin';
+
 
 function App() {
 
   return (
     <>
-      <Header/>
+    <Admin/>
+     <Routes>
+      <Route path='/Category' element={ <CategoryManagement/>} />
+      <Route path='/Product' element={ <ProductManagement/>} />
+      <Route path='/Dashboard' element={ <DashBoardManagment/>} />
+      <Route path='/Account' element={ <AdminAccount/>} />
+    </Routes>
     </>
   )
 }
